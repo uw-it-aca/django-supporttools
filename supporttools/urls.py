e3e4f7c6d4a4c6f4a722889eb888718505ae7b81
Charlon Palacay <charlon@gmail.com>
2014-04-01 14:48:14 -0700

    Adds support home url.

--- supporttools/urls.py
+++ supporttools/urls.py
@@ -1,20 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-
-    url(r'^$', include('userservice.urls')),
     
+    url(r'^users', include('userservice.urls')),
     url(r'^restclients', include('restclients.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', 'supporttools.views.home'),
     
 )
