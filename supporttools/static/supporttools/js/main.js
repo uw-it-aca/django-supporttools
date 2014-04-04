7bcb50463e52607921eccbd1ce5280db5126ce63
Charlon Palacay <charlon@gmail.com>
2014-04-04 15:48:25 -0700

    Adds support for mobile and desktop menu.

--- supporttools/static/supporttools/js/main.js
+++ supporttools/static/supporttools/js/main.js
@@ -1,6 +1,18 @@
 $(function() {
-
-
-
+        
+    // if mobile... handle menu button click fo
+    if (window.support.is_mobile) {
+        $('a#tool_menu_button').click(function(event) {
+            event.preventDefault();
+            $('#tool_menu').toggleClass('hide').toggleClass('show');
+        });
+    }
+    // else desktop... hide the menu button and show the full menu list
+    else {
+        $('a#tool_menu_button').hide();
+        $('#tool_menu').removeClass('hide');
+        
+    }
+    
 });
 
