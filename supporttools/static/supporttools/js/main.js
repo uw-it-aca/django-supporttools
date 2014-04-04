f6273c482a2d1cae347e4d6783d865e75e19eb75
Charlon Palacay <charlon@gmail.com>
2014-04-04 16:54:33 -0700

    Updates tool list padding.

--- supporttools/static/supporttools/js/main.js
+++ supporttools/static/supporttools/js/main.js
@@ -4,13 +4,13 @@ $(function() {
     if (window.support.is_mobile) {
         $('a#tool_menu_button').click(function(event) {
             event.preventDefault();
-            $('#tool_menu').toggleClass('hide').toggleClass('show');
+            $('#tool_menu').toggleClass('hide-div').toggleClass('show-div');
         });
     }
     // else desktop... hide the menu button and show the full menu list
     else {
         $('a#tool_menu_button').hide();
-        $('#tool_menu').removeClass('hide');
+        $('#tool_menu').removeClass('hide-div');
         
     }
     
