e2fb36b2ca958ea8a0dbb5b969c5887a8e9c7358
Charlon Palacay <charlon@gmail.com>
2014-04-07 14:06:36 -0700

    Adds context processor for supporttools specific stuff.

--- supporttools/context_processors.py
+++ supporttools/context_processors.py
@@ -1,15 +1,27 @@
 """ context_processors.py
 
 To make this available in your RequestContext() add
-supporttools.context_processors.is_desktop to your
-TEMPLATE_CONTEXT_PROCESSORS setting.
+
+'supporttools.context_processors.global_supportools_stuff'
+
+to yourTEMPLATE_CONTEXT_PROCESSORS setting.
 """
 from django.conf import settings
 
-def is_desktop(request):
-    """ See if the browser is desktop
-    """
-    if request.MOBILE == 0:
-        return {'is_desktop' : True}
-    else:
-        return {'is_desktop' : False}
+from restclients.pws import PWS
+from userservice.user import UserService
+
+def global_supportools_stuff(request):
+    
+    # WARNING... THIS IS NOT PRODUCTION CODE!
+    user_service = UserService()
+    user = PWS().get_person_by_netid(user_service.get_user())
+    
+    params = {
+        'supporttools_parent_app' : settings.SUPPORTTOOLS_PARENT_APP,
+        "supporttools_netid": user.uwnetid,
+    }
+
+    return (params)
+    
+    
