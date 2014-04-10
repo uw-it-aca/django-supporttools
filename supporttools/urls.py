b3ca35bfb2fc14a7c37b221f3486d778f50d80d8
Charlon Palacay <charlon@gmail.com>
2014-04-10 14:01:19 -0700

    Adds chartjs and applies status wrapper override template.

--- supporttools/urls.py
+++ supporttools/urls.py
@@ -3,9 +3,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     
-    url(r'^status', include('status_app.urls')),
     url(r'^users', include('userservice.urls')),
     url(r'^restclients', include('restclients.urls')),
+    url(r'^status', include('status_app.urls')),
     url(r'^', 'supporttools.views.home'),
     #url(r'^', include('status_app.urls')),
     
