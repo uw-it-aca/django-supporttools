4435b9bd6ae6e18229ba14c8cb5195fd249bc1af
Charlon Palacay <charlon@gmail.com>
2014-04-10 13:49:55 -0700

    Adds status app to mix and updates static url syntax on base template.

--- supporttools/urls.py
+++ supporttools/urls.py
@@ -3,8 +3,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     
+    url(r'^status', include('status_app.urls')),
     url(r'^users', include('userservice.urls')),
     url(r'^restclients', include('restclients.urls')),
     url(r'^', 'supporttools.views.home'),
+    #url(r'^', include('status_app.urls')),
     
 )
