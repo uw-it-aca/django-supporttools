42814df8fd90431a191c37398cfbdc06dac1a0a1
Charlon Palacay <charlon@gmail.com>
2014-04-23 15:27:16 -0700

    Removes PWS dependency from context processor.

--- supporttools/context_processors.py
+++ supporttools/context_processors.py
@@ -7,19 +7,16 @@ To make this available in your RequestContext() add
 to yourTEMPLATE_CONTEXT_PROCESSORS setting.
 """
 from django.conf import settings
-
-from restclients.pws import PWS
 from userservice.user import UserService
 
 def supportools_globals(request):
     
     # WARNING... THIS IS NOT PRODUCTION CODE!
     user_service = UserService()
-    user = PWS().get_person_by_netid(user_service.get_original_user())
     
     params = {
         'supporttools_parent_app' : settings.SUPPORTTOOLS_PARENT_APP,
-        'supporttools_user' : user.uwnetid,
+        'supporttools_user' : user_service.get_original_user(),
         'supporttools_is_overrideable' : settings.SUPPORTTOOLS_IS_OVERRIDEABLE,
     }
     
