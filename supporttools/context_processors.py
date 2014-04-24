ad89724a1d32cf22fbac4825f7a9fce58684c56c
Jim Laney <jlaney@uw.edu>
2014-04-24 11:58:42 -0700

    fixes issue 3

--- supporttools/context_processors.py
+++ supporttools/context_processors.py
@@ -1,27 +1,16 @@
-""" context_processors.py
-
-To make this available in your RequestContext() add
-
-'supporttools.context_processors.global_supportools_stuff'
-
-to yourTEMPLATE_CONTEXT_PROCESSORS setting.
-"""
 from django.conf import settings
 from userservice.user import UserService
 
+
 def supportools_globals(request):
-    
-    # WARNING... THIS IS NOT PRODUCTION CODE!
     user_service = UserService()
-    
     params = {
-        'supporttools_parent_app' : settings.SUPPORTTOOLS_PARENT_APP,
-        'supporttools_user' : user_service.get_original_user(),
-        'supporttools_is_overrideable' : settings.SUPPORTTOOLS_IS_OVERRIDEABLE,
+        "supporttools_parent_app": getattr(settings, "SUPPORTTOOLS_PARENT_APP", ""),
+        "supporttools_user": user_service.get_original_user(),
     }
-    
     return (params)
-    
+
+
 def has_less_compiled(request):
     """ See if django-compressor is being used to precompile less
     """ 
