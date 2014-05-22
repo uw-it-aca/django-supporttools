14cf03f2b1ef61eb7f9d73c90db06564129de3b3
Jim Laney <jlaney@uw.edu>
2014-05-22 10:08:08 -0700

    better settings name

--- supporttools/context_processors.py
+++ supporttools/context_processors.py
@@ -3,17 +3,18 @@ from userservice.user import UserService
 
 
 def supportools_globals(request):
-    user_service = UserService()
     params = {
-        "supporttools_parent_app": getattr(settings, "SUPPORTTOOLS_PARENT_APP", ""),
-        "supporttools_user": user_service.get_original_user(),
-        "app_support_views": getattr(settings, "SUPPORT_VIEWS", {}),
+        "supporttools_user": UserService().get_original_user(),
+        "supporttools_parent_app": getattr(settings,
+                                           "SUPPORTTOOLS_PARENT_APP", ""),
+        "supporttools_extra_views": getattr(settings,
+                                            "SUPPORTTOOLS_EXTRA_VIEWS", {}),
     }
     return (params)
 
 
 def has_less_compiled(request):
     """ See if django-compressor is being used to precompile less
-    """ 
+    """
     key = getattr(settings, "COMPRESS_PRECOMPILERS", None)
     return { "has_less_compiled": key != () }
