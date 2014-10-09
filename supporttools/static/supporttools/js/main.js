7b50ad95e5886ae58d105087db27517fd22eeaf0
Charlon Palacay <charlon@gmail.com>
2014-10-09 12:20:25 -0700

    Handle sidebar height calculation for desktop.

--- supporttools/static/supporttools/js/main.js
+++ supporttools/static/supporttools/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-        
+    
     // if mobile... handle menu button click fo
     if (window.support.is_mobile || window.support.is_tablet) {
         $('a#tool_menu_button').click(function(event) {
@@ -12,7 +12,29 @@ $(function() {
         $('a#tool_menu_button').hide();
         $('#tool_menu').removeClass('hide-div');
         
+        // get correct height for sidebar
+        calculate_sidebar_height();
+        
     }
     
 });
 
+$(window).resize(function() {
+    
+    // get correct height for sidebar
+    calculate_sidebar_height();
+});
+
+
+function calculate_sidebar_height() {
+    
+    var base_h = $(".header").height();
+    var header_h = 60;
+    var netid_h = $(".tool-app").outerHeight();
+    var footer_h = $(".footer").outerHeight(); 
+    
+    var diff_h = base_h - (header_h + netid_h + footer_h) - 20;
+    
+    $(".tool-list-inner").height(diff_h);
+
+}
