f4576eb2652975b98846d1ec5c8c59adf33b4187
Fang Lin <fanglin@uw.edu>
2016-10-31 11:49:51 -0700

    use name of url-patterns

--- supporttools/urls.py
+++ supporttools/urls.py
@@ -4,6 +4,6 @@ from supporttools.views import home
 urlpatterns = [
     # for now... make the home view the default landing for
     # /support
-    url(r'^', home),
+    url(r'^', home, name='supporttools_home'),
     # url(r'^', include('status_app.urls')),
     ]
