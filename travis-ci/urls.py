c465bdc7d938f323188204e63203ee1be5b144aa
Bradley Bell <bradleyb@uw.edu>
2018-01-22 18:54:03 -0800

    remove travis dependency on oauth_provider and nagios_registration

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^oauth/', include('oauth_provider.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('nagios_registration.urls')),
     ]
