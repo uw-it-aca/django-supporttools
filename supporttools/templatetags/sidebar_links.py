7bd1a2930d851d1c4ae33863cf6d43c091327a4a
Ethan Turner <ezturner@washington.edu>
2018-09-11 11:49:46 -0700

    Upgrading string formatting

--- supporttools/templatetags/sidebar_links.py
+++ supporttools/templatetags/sidebar_links.py
@@ -11,6 +11,6 @@ def do_sidebar_links(parser, token):
     default_template = "supporttools/default_sidelinks.html"
     try:
         template.loader.get_template(custom_template)
-        return IncludeNode(parser.compile_filter("'%s'" % custom_template))
+        return IncludeNode(parser.compile_filter("'{}'".format(custom_template)))
     except template.TemplateDoesNotExist:
-        return IncludeNode(parser.compile_filter("'%s'" % default_template))
+        return IncludeNode(parser.compile_filter("'{}'".format(default_template)))
