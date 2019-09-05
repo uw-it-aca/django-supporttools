a356a56008a475cddd78ce9a50639e5413bf4962
mike seibel <mikes@washington.edu>
2019-09-05 12:10:32 -0700

    conditional compress

--- supporttools/views.py
+++ supporttools/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def home(request):
-    return render(request, 'supporttools/home.html', {})
+    return render(request, 'supporttools/home.html', {
+        'has_compress': ('compress' in settings.INSTALLED_APPS)
+    })
