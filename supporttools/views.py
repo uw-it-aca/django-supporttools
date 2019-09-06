42f43fcf4b3d5ff633dd1bfbb167fd9da5db4998
mike seibel <mikes@washington.edu>
2019-09-06 15:02:32 -0700

    no compress test

--- supporttools/views.py
+++ supporttools/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.conf import settings
 
 
 def home(request):
-    return render(request, 'supporttools/home.html', {
-        'has_compress': ('compress' in settings.INSTALLED_APPS)
-    })
+    return render(request, 'supporttools/home.html', {})
